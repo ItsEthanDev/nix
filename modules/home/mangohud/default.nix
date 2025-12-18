d85cd2b735e875aab2672b16934b70cde49d0a08
EthanJ-Brady <git@ethanbrady.xyz>
2025-12-17 21:18:23 -0700

    Configured display variables for mangohud

--- modules/home/mangohud/default.nix
+++ modules/home/mangohud/default.nix
@@ -3,10 +3,18 @@
     enable = true;
     enableSessionWide = true;
     settings = {
-      full = true;
-      gamemode = true;
       no_display = true;
       toggle_hud = "Super_L+Control_L+H";
+
+      gpu_temp = true;
+      display_server = true;
+      fan = true;
+      frametime = true;
+      gpu_fan = true;
+      ram = true;
+      vram = true;
+      wine = true;
+      gamemode = true;
     };
   };
 }
