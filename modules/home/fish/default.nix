53b58507e942753ab602cb8c07ae77a03c32c87e
Ethan <git@ethanbrady.xyz>
2025-12-23 02:24:47 -0700

    Added ctrl y for autocomplete

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -1,6 +1,12 @@
 {...}: {
   programs.fish = {
     enable = true;
+    binds = {
+      "\\cy" = {
+        command = "accept-autosuggestion";
+        mode = "insert";
+      };
+    };
     interactiveShellInit = ''
       fish_vi_key_bindings
     '';
