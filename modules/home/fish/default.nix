b411e382b51f71946573df4afc967815b9424686
Ethan <git@ethanbrady.xyz>
2025-12-26 14:01:18 -0700

    Added comments and conditionals to the fish and ghostty config

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -8,8 +8,13 @@
       };
     };
     interactiveShellInit = ''
+      # Vi bindings
       fish_vi_key_bindings
+
+      # Disables the startup greeting
       set fish_greeting
+
+      # Sets the SHELL env variable to the fish location
       set -gx SHELL (which fish)
     '';
   };
