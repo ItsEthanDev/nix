8eadca08e00f5fe499368ff21d72f99895f1c941
Ethan <git@ethanbrady.xyz>
2025-12-30 02:51:44 -0700

    Updated the way starship looks when connected over ssh

--- modules/home/starship/default.nix
+++ modules/home/starship/default.nix
@@ -8,7 +8,9 @@
       bun.disabled = true;
       cmd_duration.disabled = true;
       gcloud.disabled = true;
+      hostname.ssh_symbol = "";
       nodejs.disabled = true;
+      username.disabled = true;
     };
   };
 }
