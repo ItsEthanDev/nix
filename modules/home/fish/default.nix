10ca8e9de17514cce5e3a457dd4d0409a106a62e
ItsEthanXYZ <git@ethanbrady.xyz>
2025-12-31 10:38:11 -0700

    Added !! for previous command expansion to fish

--- modules/home/fish/default.nix
+++ modules/home/fish/default.nix
@@ -16,6 +16,15 @@
 
       # Sets the SHELL env variable to the fish location
       set -gx SHELL (which fish)
+
+      # History expansion with !!
+      function last_history_item
+        echo $history[1]
+      end
+      abbr -a !! --position anywhere --function last_history_item
     '';
+    shellAbbrs = {
+      "!!" = "'history | head -1 | read cmd; eval $cmd'";
+    };
   };
 }
