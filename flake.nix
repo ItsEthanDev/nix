f9d80db7c8583b6cdc045c71729d89c45e301aef
ItsEthanXYZ <git@ethanbrady.xyz>
2026-01-06 14:50:01 -0700

    Had home manager use global packages

--- flake.nix
+++ flake.nix
@@ -103,6 +103,8 @@
           home-manager.nixosModules.home-manager
           {
             home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
               extraSpecialArgs = {
                 inherit inputs;
               };
