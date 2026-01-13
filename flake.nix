bf41f3cb42a6f4fb3eb4711b90c0d6921e265b6f
ItsEthanXYZ <git@ethanbrady.xyz>
2026-01-12 21:07:29 -0700

    Updated the flake with nixos-unstable

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     catppuccin.url = "github:catppuccin/nix";
     nix-gaming.url = "github:fufexan/nix-gaming";
