c3771d54f5cbe81a45c3a697003bfe084edc2b7b
Ethan Brady <git@ethanbrady.xyz>
2026-01-25 03:12:25 -0700

    Opened firewall for hytale

--- systems/x86_64-linux/turing/default.nix
+++ systems/x86_64-linux/turing/default.nix
@@ -3,6 +3,11 @@
     ./hardware-configuration.nix
   ];
 
+  networking.firewall.enable = true;
+
+  networking.firewall.allowedTCPPorts = [5520];
+  networking.firewall.allowedUDPPorts = [5520];
+
   networking.hostName = "turing";
   nixpkgs.system = "x86_64-linux";
   system.stateVersion = "25.11";
