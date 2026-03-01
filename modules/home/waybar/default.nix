b9b29b3b377ecfd31817cdb11d2e5e990cc9f5ee
Ethan Brady <git@ethanbrady.xyz>
2026-03-01 00:27:48 -0700

    Refactored the waybar module

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -1,25 +1,16 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }: {
   imports = [
     ./settings.nix
-  ];
-
-  home.packages = with pkgs; [
-    btop
-    pamixer
-    wiremix
+    ./style.nix
   ];
 
   wayland.windowManager.hyprland.settings = lib.mkIf config.wayland.windowManager.hyprland.enable {
     "exec-once" = ["waybar"];
   };
 
-  programs.waybar = {
-    enable = true;
-    style = builtins.readFile ./style.css;
-  };
+  programs.waybar.enable = true;
 }
