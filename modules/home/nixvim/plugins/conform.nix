5ebdfcb569db114f9cc3eadc9044e7f43748f20d
Ethan Brady <newton@itsEthan.dev>
2026-05-06 22:45:04 -0600

    Added some additional formatter keybinds

--- modules/home/nixvim/plugins/conform.nix
+++ modules/home/nixvim/plugins/conform.nix
@@ -1,49 +1,102 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.lib.nixvim) mkRaw;
+in {
   home.packages = with pkgs; [
     biome
     rustfmt
     alejandra
   ];
 
-  programs.nixvim.plugins.conform-nvim = {
-    enable = true;
-    settings = {
-      format_on_save = {
-        lsp_format = "fallback";
-        timeout_ms = 2000;
-        stop_after_first = true;
+  programs.nixvim = {
+    globals.autoformat = true;
+
+    keymaps = [
+      {
+        key = "<leader>uf";
+        mode = "n";
+        action = mkRaw ''
+          function()
+            vim.g.autoformat = not vim.g.autoformat
+            vim.notify("Auto Format (Global): " .. (vim.g.autoformat and "On" or "Off"))
+          end
+        '';
+        options = {
+          desc = "Toggle Auto Format (Global)";
+          silent = true;
+        };
+      }
+      {
+        key = "<leader>uF";
+        mode = "n";
+        action = mkRaw ''
+          function()
+            local enabled = vim.b.autoformat
+            if enabled == nil then
+              enabled = vim.g.autoformat ~= false
+            end
+            vim.b.autoformat = not enabled
+            vim.notify("Auto Format (Buffer): " .. (vim.b.autoformat and "On" or "Off"))
+          end
+        '';
+        options = {
+          desc = "Toggle Auto Format (Buffer)";
+          silent = true;
+        };
+      }
+    ];
+
+    plugins.conform-nvim = {
+      enable = true;
+      settings = {
+        format_on_save = mkRaw ''
+          function(bufnr)
+            if vim.g.autoformat == false or vim.b[bufnr].autoformat == false then
+              return
+            end
+
+            return {
+              lsp_format = "fallback",
+              timeout_ms = 2000,
+              stop_after_first = true,
+            }
+          end
+        '';
+        formatters_by_ft = {
+          javascript = [
+            "biome"
+          ];
+          javascriptreact = [
+            "biome"
+          ];
+          json = [
+            "biome"
+          ];
+          markdown = [
+            "biome"
+          ];
+          nix = [
+            "alejandra"
+          ];
+          python = [
+            "black"
+          ];
+          rust = [
+            "rustfmt"
+          ];
+          typescript = [
+            "biome"
+          ];
+          typescriptreact = [
+            "biome"
+          ];
+        };
+        notify_on_error = false;
+        notify_no_formatters = false;
       };
-      formatters_by_ft = {
-        javascript = [
-          "biome"
-        ];
-        javascriptreact = [
-          "biome"
-        ];
-        json = [
-          "biome"
-        ];
-        markdown = [
-          "biome"
-        ];
-        nix = [
-          "alejandra"
-        ];
-        python = [
-          "black"
-        ];
-        rust = [
-          "rustfmt"
-        ];
-        typescript = [
-          "biome"
-        ];
-        typescriptreact = [
-          "biome"
-        ];
-      };
-      notify_on_error = false;
-      notify_no_formatters = false;
     };
   };
 }
