4d4a6a980d091f097193a1a080bca0ad41bf2e6a
Ethan Brady <newton@itsEthan.dev>
2026-05-06 22:46:26 -0600

    Added some lsp config

--- modules/home/nixvim/plugins/lsp.nix
+++ modules/home/nixvim/plugins/lsp.nix
@@ -1,39 +1,28 @@
 _: {
-  programs.nixvim.plugins = {
-    cmp = {
-      enable = true;
-      autoEnableSources = true;
-      settings = {
-        experimental.ghost_text = true;
-        mapping = {
-          "<C-Space>" = "cmp.mapping.complete()";
-          "<C-n>" = "cmp.mapping.select_next_item()";
-          "<C-p>" = "cmp.mapping.select_prev_item()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-          "<C-f>" = "cmp.mapping.scroll_docs(4)";
-          "<C-y>" = "cmp.mapping.confirm({ select = true })";
+  programs.nixvim.plugins.lsp = {
+    enable = true;
+    servers = {
+      # CSS
+      cssls.enable = true;
+      # JSON
+      jsonnet_ls.enable = true;
+      # Nix
+      nixd = {
+        enable = true;
+        config = {
+          nixpkgs.expr = "import <nixpkgs> { }";
         };
-        sources = [
-          {name = "nvim_lsp";}
-          {name = "luasnip";}
-          {name = "path";}
-        ];
       };
-    };
-    lsp = {
-      enable = true;
-      servers = {
-        jsonnet_ls.enable = true;
-        nixd = {
-          enable = true;
-          settings = {
-            nixpkgs.expr = "import <nixpkgs> { }";
-          };
-        };
-        rust_analyzer.enable = true;
-        tailwindcss.enable = true;
-        ts_ls.enable = true;
+      # Rust
+      rust_analyzer = {
+        enable = true;
+        installRustc = true;
+        installCargo = true;
       };
+      # Tailwind
+      tailwindcss.enable = true;
+      # Typescript
+      ts_ls.enable = true;
     };
   };
 }
