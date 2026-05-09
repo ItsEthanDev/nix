0cb5baa9a0b9a9c5774aad7e53cb296d00737b7b
Ethan Brady <turing@itsEthan.dev>
2026-05-09 12:14:22 -0600

    Set default audio volume for opening new apps to 50%

--- modules/nixos/audio/default.nix
+++ modules/nixos/audio/default.nix
@@ -7,6 +7,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    wireplumber.extraConfig."default-app-volume" = {
+      "wireplumber.settings" = {
+        "node.stream.default-playback-volume" = 0.125;
+      };
+    };
   };
 
   # Necessary for the easy-effects daemon to work correctly
