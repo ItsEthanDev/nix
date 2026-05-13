f519660770af02c7143a911dae0f7e9660ab7e30
Ethan Brady <turing@itsEthan.dev>
2026-05-13 01:06:40 -0600

    set hyprcursor enable option which automatically sets the env vars

--- modules/home/banana-cursor/default.nix
+++ modules/home/banana-cursor/default.nix
@@ -2,15 +2,9 @@
   home.pointerCursor = {
     gtk.enable = true;
     x11.enable = true;
+    hyprcursor.enable = true;
     name = "Banana";
     package = pkgs.banana-cursor;
     size = 24;
-  };
-
-  wayland.windowManager.hyprland.settings = {
-    env = [
-      "HYPRCURSOR_THEME,banana"
-      "HYPRCURSOR_SIZE,24"
-    ];
   };
 }
