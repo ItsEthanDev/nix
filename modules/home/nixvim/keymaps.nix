6757242ba79a9b781569e954ff121f54d276d540
Ethan Brady <newton@itsEthan.dev>
2026-05-17 15:06:03 -0600

    Improved ui of the go to definition and go to references viewing

--- modules/home/nixvim/keymaps.nix
+++ modules/home/nixvim/keymaps.nix
@@ -1,5 +1,25 @@
 {config, ...}: let
   inherit (config.lib.nixvim) mkRaw;
+
+  lspPicker = picker: title:
+    mkRaw ''
+      function()
+        require("telescope.builtin").${picker}({
+          prompt_title = "${title}",
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
+          layout_config = {
+            width = 0.8,
+            height = 0.9,
+            prompt_position = "top",
+            preview_cutoff = 20,
+            preview_height = function(_, _, max_lines)
+              return max_lines - 15
+            end,
+          },
+        })
+      end
+    '';
 in {
   programs.nixvim = {
     keymaps = [
@@ -177,19 +197,29 @@ in {
           silent = true;
         };
       }
+      {
+        action = lspPicker "lsp_definitions" "Definitions";
+        key = "gd";
+        mode = "n";
+        options = {
+          desc = "Goto Definition";
+          silent = true;
+        };
+      }
+      {
+        action = lspPicker "lsp_references" "References";
+        key = "gr";
+        mode = "n";
+        options = {
+          desc = "References";
+          silent = true;
+        };
+      }
     ];
     lsp.keymaps = [
       {
-        key = "gd";
-        lspBufAction = "definition";
-      }
-      {
         key = "gD";
         lspBufAction = "declaration";
-      }
-      {
-        key = "gr";
-        lspBufAction = "references";
       }
       {
         key = "gy";
