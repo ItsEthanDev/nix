d9d504bdc5d4ee222011113ef35374d563899051
Ethan Brady <newton@itsEthan.dev>
2026-05-24 19:12:42 -0600

    Updated the rust package to install the right packages on the right systems

--- modules/home/rust/default.nix
+++ modules/home/rust/default.nix
@@ -1,8 +1,18 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    cargo
-    rustc
-    gcc
-    pkg-config
-  ];
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (pkgs.stdenv) isLinux;
+in {
+  home.packages = with pkgs;
+    [
+      cargo
+      rustc
+      pkg-config
+    ]
+    # We'll only install this on Linux because MacOS comes preloaded with a compiler
+    ++ lib.optionals isLinux [
+      gcc
+    ];
 }
