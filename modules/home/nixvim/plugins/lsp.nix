09486efe12be2a45ee82b15f46d254920124a539
Ethan Brady <newton@itsEthan.dev>
2026-05-24 19:05:38 -0600

    Removed no longer used lsp config

--- modules/home/nixvim/plugins/lsp.nix
+++ modules/home/nixvim/plugins/lsp.nix
@@ -1,29 +1,5 @@
-{config, ...}: let
-  inherit (config.lib.nixvim) mkRaw;
-in {
+_: {
   programs.nixvim.plugins = {
-    # actions-preview = {
-    #   enable = true;
-    #   settings = {
-    #     backend = ["telescope"];
-    #     telescope = mkRaw ''
-    #       vim.tbl_extend("force", require("telescope.themes").get_dropdown(), {
-    #         sorting_strategy = "ascending",
-    #         layout_strategy = "vertical",
-    #         layout_config = {
-    #           width = 0.8,
-    #           height = 0.9,
-    #           prompt_position = "top",
-    #           preview_cutoff = 20,
-    #           preview_height = function(_, _, max_lines)
-    #             return max_lines - 15
-    #           end,
-    #         },
-    #       })
-    #     '';
-    #   };
-    # };
-
     lsp = {
       enable = true;
       servers = {
