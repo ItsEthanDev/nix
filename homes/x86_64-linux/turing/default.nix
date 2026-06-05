74318efc16158f7a7e553a130f2f6f8f0aed588a
Ethan Brady <turing@itsEthan.dev>
2026-06-04 23:32:37 -0600

    Setup Qwen3.6 as a model I can use in opencode on turing

--- homes/x86_64-linux/turing/default.nix
+++ homes/x86_64-linux/turing/default.nix
@@ -24,6 +24,23 @@
     ];
   };
 
+  programs.opencode = {
+    settings.provider = {
+      ollama = {
+        npm = "@ai-sdk/openai-compatible";
+        name = "Ollama (local)";
+        options = {
+          baseURL = "http://localhost:11434/v1";
+        };
+        models = {
+          "qwen3.6" = {
+            name = "Qwen3.6";
+          };
+        };
+      };
+    };
+  };
+
   nixpkgs.config.allowUnfree = true;
   home = {
     username = "ethan";
