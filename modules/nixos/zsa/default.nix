3cd9ccdfcbbefdf535a6a2ece9dbf3b05572938f
Ethan Brady <turing@itsEthan.dev>
2026-06-10 20:33:19 -0600

    Installed zapp with the zsa module

--- modules/nixos/zsa/default.nix
+++ modules/nixos/zsa/default.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.zapp.nixosModules.default
+  ];
+
   hardware.keyboard.zsa.enable = true;
   environment.systemPackages = with pkgs; [
     keymapp
   ];
+  programs.zapp.enable = true;
 }
