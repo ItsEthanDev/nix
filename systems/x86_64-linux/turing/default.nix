f7a1c263d4ad47c488399ecf74711674383b7386
Ethan Brady <turing@itsEthan.dev>
2026-06-18 22:38:30 -0600

    Packed a nested set

--- systems/x86_64-linux/turing/default.nix
+++ systems/x86_64-linux/turing/default.nix
@@ -3,10 +3,13 @@ _: {
     ./hardware-configuration.nix
   ];
 
-  networking.firewall.enable = true;
-
-  networking.firewall.allowedTCPPorts = [5520 38281];
-  networking.firewall.allowedUDPPorts = [5520 38281];
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [5520 38281];
+      allowedUDPPorts = [5520 38281];
+    };
+  };
 
   my = {
     gaming.enable = true;
