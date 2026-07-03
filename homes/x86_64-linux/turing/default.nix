6e3a970f9b83482de8c3bc4ce5317a19b27e5f66
Ethan Brady <turing@itsEthan.dev>
2026-07-02 23:01:41 -0600

    Reoganized the home module for turing

--- homes/x86_64-linux/turing/default.nix
+++ homes/x86_64-linux/turing/default.nix
@@ -3,46 +3,6 @@
   pkgs,
   ...
 }: {
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "DP-4,2560x1440@164.80,1920x-360,1"
-      "HDMI-A-1,1920x1080@60.00,0x0,1"
-      ",preferred,auto,1"
-    ];
-
-    workspace = [
-      "1, monitor:DP-4, default:true"
-      "2, monitor:DP-4"
-      "3, monitor:DP-4"
-      "4, monitor:DP-4"
-      "5, monitor:DP-4"
-      "6, monitor:HDMI-A-1"
-      "7, monitor:HDMI-A-1"
-      "8, monitor:HDMI-A-1"
-      "9, monitor:HDMI-A-1"
-      "0, monitor:HDMI-A-1"
-    ];
-  };
-
-  my.desktop.monitors.primary = "DP-4";
-
-  programs.opencode = {
-    settings.provider = {
-      ollama = {
-        npm = "@ai-sdk/openai-compatible";
-        name = "Ollama (local)";
-        options = {
-          baseURL = "http://localhost:11434/v1";
-        };
-        models = {
-          "qwen3.6" = {
-            name = "Qwen3.6";
-          };
-        };
-      };
-    };
-  };
-
   gtk.enable = true;
 
   home = {
@@ -76,23 +36,66 @@
     ];
   };
 
+  my.desktop.monitors.primary = "DP-4";
+
+  programs = {
+    home-manager.enable = true;
+    opencode = {
+      settings.provider = {
+        ollama = {
+          npm = "@ai-sdk/openai-compatible";
+          name = "Ollama (local)";
+          options = {
+            baseURL = "http://localhost:11434/v1";
+          };
+          models = {
+            "qwen3.6" = {
+              name = "Qwen3.6";
+            };
+          };
+        };
+      };
+    };
+  };
+
   services = {
     easyeffects.enable = true;
     ollama.enable = true;
   };
 
-  programs.home-manager.enable = true;
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "DP-4,2560x1440@164.80,1920x-360,1"
+      "HDMI-A-1,1920x1080@60.00,0x0,1"
+      ",preferred,auto,1"
+    ];
 
-  xdg.desktopEntries.nemo = {
-    name = "Nemo";
-    exec = lib.getExe' pkgs.nemo-with-extensions "nemo";
+    workspace = [
+      "1, monitor:DP-4, default:true"
+      "2, monitor:DP-4"
+      "3, monitor:DP-4"
+      "4, monitor:DP-4"
+      "5, monitor:DP-4"
+      "6, monitor:HDMI-A-1"
+      "7, monitor:HDMI-A-1"
+      "8, monitor:HDMI-A-1"
+      "9, monitor:HDMI-A-1"
+      "0, monitor:HDMI-A-1"
+    ];
   };
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-      "inode/directory" = ["nemo.desktop"];
-      "application/x-gnome-saved-search" = ["nemo.desktop"];
+
+  xdg = {
+    desktopEntries.nemo = {
+      name = "Nemo";
+      exec = lib.getExe' pkgs.nemo-with-extensions "nemo";
     };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "inode/directory" = ["nemo.desktop"];
+        "application/x-gnome-saved-search" = ["nemo.desktop"];
+      };
+    };
+    userDirs.enable = true;
   };
-  xdg.userDirs.enable = true;
 }
