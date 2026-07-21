64f0bf4dd15a0498df9eae9065b084fc3fbb308d
Ethan Brady <turing@itsEthan.dev>
2026-07-21 00:59:50 -0600

    Added restic backups

--- systems/x86_64-linux/turing/default.nix
+++ systems/x86_64-linux/turing/default.nix
@@ -150,6 +150,37 @@ in {
       settings.session_log = null;
     };
     flatpak.enable = true;
+    restic.backups.r2 = {
+      repository = "s3:https://7be572932f308ef922603015d775fdd2.r2.cloudflarestorage.com/backup/turing";
+      environmentFile = "/etc/restic/r2.env";
+      passwordFile = "/etc/restic/password";
+
+      paths = [
+        "/home/ethan/Pictures/Screenshots"
+        "/home/ethan/Documents"
+      ];
+      exclude = [];
+
+      extraOptions = [
+        "s3.bucket-lookup=path"
+      ];
+
+      timerConfig = {
+        OnCalendar = "*-*-* 03:00:00";
+        RandomizedDelaySec = "1h";
+        Persistent = true;
+      };
+
+      pruneOpts = [
+        "--keep-daily 14"
+        "--keep-weekly 8"
+        "--keep-monthly 12"
+        "--keep-yearly 3"
+      ];
+
+      inhibitsSleep = true;
+      initialize = true;
+    };
     sunshine = {
       enable = true;
       autoStart = true;
