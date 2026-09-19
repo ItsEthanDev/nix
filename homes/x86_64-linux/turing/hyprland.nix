a5021f4bac18e81da8695058780c6efa162e8da2
Ethan Brady <turing@itsEthan.dev>
2026-09-19 15:22:02 -0600

    feat(hyprland): enable fullscreen VRR

--- homes/x86_64-linux/turing/hyprland.nix
+++ homes/x86_64-linux/turing/hyprland.nix
@@ -28,6 +28,7 @@ in {
     systemd.enable = false;
     settings = {
       "misc:mouse_move_enables_dpms" = true;
+      "misc:vrr" = 2;
 
       exec-once = lib.mkAfter [
         "${lib.getExe pkgs.xrandr} --output DP-4 --primary"
